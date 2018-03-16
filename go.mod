b19e6c7fc9ce2246e3ac59b1240ce2f7a75ddede
sam boyer <tech@samboyer.org>
2018-03-16 01:00:07 -0400

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module "github.com/sdboyer/vgotest2"
+
+require "github.com/sdboyer/vgotest1" v1.0.1
---
+++ lib.go
@@ -0,0 +1,5 @@
+package vgotest2 // import "github.com/sdboyer/vgotest2"
+
+import "github.com/sdboyer/vgotest1"
+
+type Test2Type vgotest1.Test1Type
